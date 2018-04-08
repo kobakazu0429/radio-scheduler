c21784bfc1f621e32988c86de25ee2170abf4d13
kobakakzu0429 <kobakazu10@gmail.com>
2018-04-08 22:11:24 +0900

    use axios

--- src/compornents/form.vue
+++ src/compornents/form.vue
@@ -1,62 +1,62 @@
 <template>
   <div id="modal1" class="modal modal-fixed-footer">
-    <form action="https://script.google.com/macros/s/AKfycbzub6RPKKORwtpMpqccA7Q5mzhNQ_HnFzBjYl74pORaaM3u-39X/exec" method="post">
+    <form>
       <div class="modal-content">
         <div class="row" id="title-row">
           <div class="input-field col s12">
-            <input id="title" type="text" class="validate" name="title">
+            <input id="title" type="text" class="validate" v-model="title">
             <label for="title">タイトル</label>
           </div>
         </div>
         <div class="row">
-          <div class="col s12"><label for="published_at">公開日</label><input type="date" id="published_at" name="published_at"></div>
+          <div class="col s12"><label for="published_at">公開日</label><input type="date" id="published_at" v-model="published_at"></div>
           <div class="col s2">
             <label for="rec">収録</label>
           </div>
           <div class="col s10">
-            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" id="rec" name="rec"><span class="lever"></span><i class="fas fa-check"></i></label></div>
+            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" false-value='false' true-value='true' id="rec" v-model="rec"><span class="lever"></span><i class="fas fa-check"></i></label></div>
           </div>
           <div class="col s2">
             <label for="edit">編集</label>
           </div>
           <div class="col s10">
-            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" id="edit" name="edit"><span class="lever"></span><i class="fas fa-check"></i></label></div>
+            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" false-value='false' true-value='true' id="edit" v-model="edit"><span class="lever"></span><i class="fas fa-check"></i></label></div>
           </div>
           <div class="col s2">
             <label for="censorship">検閲</label>
           </div>
           <div class="col s10">
-            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" id="censorship" name="censorship"><span class="lever"></span><i class="fas fa-check"></i></label></div>
+            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" false-value='false' true-value='true' id="censorship" v-model="censorship"><span class="lever"></span><i class="fas fa-check"></i></label></div>
           </div>
           <div class="col s2">
-            <label for="thumb">サムネイル画像</label>
+            <label for="thumbnail">サムネイル画像</label>
           </div>
           <div class="col s10">
-            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" id="thumb" name="thumb"><span class="lever"></span><i class="fas fa-check"></i></label></div>
+            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" false-value='false' true-value='true' id="thumbnail" v-model="thumbnail"><span class="lever"></span><i class="fas fa-check"></i></label></div>
           </div>
           <div class="col s2">
             <label for="reserve">予約投稿</label>
           </div>
           <div class="col s10">
-            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" id="reserve" name="reserve"><span class="lever"></span><i class="fas fa-check"></i></label></div>
+            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" false-value='false' true-value='true' id="reserve" v-model="reserve"><span class="lever"></span><i class="fas fa-check"></i></label></div>
           </div>
           <div class="col s2">
             <label for="release">公開</label>
           </div>
           <div class="col s10">
-            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" id="release" name="release"><span class="lever"></span><i class="fas fa-check"></i></label></div>
+            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" false-value='false' true-value='true' id="release" v-model="release"><span class="lever"></span><i class="fas fa-check"></i></label></div>
           </div>
           <div class="col s2">
             <label for="comic">4コマ漫画</label>
           </div>
           <div class="col s10">
-            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" id="comic" name="comic"><span class="lever"></span><i class="fas fa-check"></i></label></div>
+            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" false-value='false' true-value='true' id="comic" v-model="comic"><span class="lever"></span><i class="fas fa-check"></i></label></div>
           </div>
           <div class="col s2">
             <label for="tweet">ツイート</label>
           </div>
           <div class="col s10">
-            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" id="tweet" name="tweet"><span class="lever"></span><i class="fas fa-check"></i></label></div>
+            <div class="switch"><label><i class="fas fa-times"></i><input type="checkbox" false-value='false' true-value='true' id="tweet" v-model="tweet"><span class="lever"></span><i class="fas fa-check"></i></label></div>
           </div>
         </div>
         <div class="row upload-row">
@@ -64,15 +64,15 @@
             <label for="rec_source">ラジオ音源</label>
           </div>
           <div class="col s10">
-            <input type="file" id="rec_source" name="rec_source" @change="convert('rec_source')">
+            <input type="file" id="rec_source" @change="convert('rec_source')">
           </div>
         </div>
         <div class="row upload-row">
           <div class="col s2">
-            <label for="thumb_img">サムネイル画像</label>
+            <label for="thumbnail_img">サムネイル画像</label>
           </div>
           <div class="col s10">
-            <input type="file" id="thumb_img" name="thumb_img" @change="convert('thumb_img')">
+            <input type="file" id="thumbnail_img" @change="convert('thumbnail_img')">
           </div>
         </div>
         <div class="row upload-row">
@@ -80,14 +80,14 @@
             <label for="comic_img">4コマ漫画</label>
           </div>
           <div class="col s10">
-            <input type="file" id="comic_img" name="comic_img" @change="convert('comic_img')">
+            <input type="file" id="comic_img" @change="convert('comic_img')">
           </div>
         </div>
       </div>
       <div class="modal-footer">
         <div class="row">
           <div class="col s12">
-            <button type="button" class="modal-action modal-close waves-effect waves-green btn-flat">追加する</button>
+            <button type="button" class="modal-action modal-close waves-effect waves-green btn-flat" @click=send()>追加する</button>
             <button type="button" class="modal-action modal-close waves-effect waves-green btn-flat">キャンセル</button>
           </div>
         </div>
@@ -98,6 +98,23 @@
 
 <script>
   module.exports = {
+    data: function() {
+      return {
+        title: '',
+        published_at: '',
+        rec: false,
+        edit: false,
+        censorship: false,
+        thumbnail: false,
+        reserve: false,
+        release: false,
+        comic: false,
+        tweet: false,
+        rec_source: '',
+        thumbnail_img: '',
+        comic_img: '',
+      }
+    },
     methods: {
       convert: function(target_id) {
         var input_file = document.getElementById(target_id);
@@ -126,16 +143,37 @@
         // 読み込み成功時に実行されるイベント
         file_reader.onload = function(e){
 
-          // 出力テスト
-          console.log(file_reader.result);
-        };
+        // 出力テスト
+        console.log(file_reader.result);
+      };
 
-        // ------------------------------------------------------------
-        // 読み込みを開始する（Data URI Scheme 文字列を得る）
-        file_reader.readAsDataURL(file);
-      }
+      // ------------------------------------------------------------
+      // 読み込みを開始する（Data URI Scheme 文字列を得る）
+      file_reader.readAsDataURL(file);
+    },
+    send: function() {
+      this.axios({
+        method: 'post',
+        url: 'https://script.google.com/macros/s/AKfycbzub6RPKKORwtpMpqccA7Q5mzhNQ_HnFzBjYl74pORaaM3u-39X/exec',
+        data: {
+          title       : this.title.value,
+          published_at: this.published_at.value,
+          rec         : this.rec,
+          edit        : this.edit,
+          censorship  : this.censorship,
+          thumbnail   : this.thumbnail,
+          reserve     : this.reserve,
+          release     : this.release,
+          comic       : this.comic,
+          tweet       : this.tweet,
+        },
+        headers: {
+          'Content-Type': 'text/plain;charset=utf-8',
+        }
+      })
     }
   }
+}
 </script>
 
 <style scoped>
